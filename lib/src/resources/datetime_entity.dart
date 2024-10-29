6e151841b885e2ce883a25a853250394aad58cc7
herbsun <herbsun@163.com>
2024-10-30 00:01:19 +0800

    feat: add birthday view

--- lib/src/resources/datetime_entity.dart
+++ lib/src/resources/datetime_entity.dart
@@ -14,8 +14,6 @@ class DateTimeEntity {
   String? lunarDayName;
   String? zodiac;
 
-
-
   DateTimeEntity({
     this.year,
     this.month,
@@ -31,15 +29,34 @@ class DateTimeEntity {
   });
 
   static DateTimeEntity fromDateTime(DateTime dateTime) => DateTimeEntity(
-    year: dateTime.year,
-    month: dateTime.month,
-    day: dateTime.day,
-    hour: dateTime.hour,
-    minute: dateTime.minute,
-    second: dateTime.second,
-    lunarYearName: getLunarYearName(dateTime.year),
-    lunarMonthName: getLunarMonthName(dateTime.month),
-    lunarDayName: getLunarDayName(dateTime.day),
-  );
-  
+        year: dateTime.year,
+        month: dateTime.month,
+        day: dateTime.day,
+        hour: dateTime.hour,
+        minute: dateTime.minute,
+        second: dateTime.second,
+        lunarYearName: getLunarYearName(dateTime.year),
+        lunarMonthName: getLunarMonthName(dateTime.month),
+        lunarDayName: getLunarDayName(dateTime.day),
+      );
+
+  static DateTimeEntity fromYMDHMS({
+    int? year,
+    int? month,
+    int? day,
+    int? hour,
+    int? minute,
+    int? second,
+  }) =>
+      DateTimeEntity(
+        year: year,
+        month: month,
+        day: day,
+        hour: hour,
+        minute: minute,
+        second: second,
+        lunarYearName: year != null ? getLunarYearName(year) : null,
+        lunarMonthName: month != null ? getLunarMonthName(month) : null,
+        lunarDayName: day != null ? getLunarDayName(day) : null,
+      );
 }
